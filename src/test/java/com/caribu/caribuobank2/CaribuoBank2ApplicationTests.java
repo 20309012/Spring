1f0990dcf4c36d9a6375fdbd82e79640aac9bff2
abasalt <a.basalt.72@gmail.com>
2025-02-09 22:49:12 +0330

    done progect

---
+++ src/test/java/com/caribu/caribuobank2/CaribuoBank2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caribu.caribuobank2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaribuoBank2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
